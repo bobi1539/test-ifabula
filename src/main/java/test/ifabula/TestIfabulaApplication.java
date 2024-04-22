c43e7d0ca9f49486fd0b2ce370c508a605a949aa
bobi.rival <bahmadrival@gmail.com>
2024-04-22 13:01:20 +0700

    Init project

---
+++ src/main/java/test/ifabula/TestIfabulaApplication.java
@@ -0,0 +1,13 @@
+package test.ifabula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIfabulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIfabulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/ifabula/TestIfabulaApplicationTests.java
@@ -0,0 +1,13 @@
+package test.ifabula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIfabulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
